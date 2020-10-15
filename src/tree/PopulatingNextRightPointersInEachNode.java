acc893cb0199a7ed441e9b3481e049fc4a679a0e
hexi007 <2777125829@qq.com>
2020-10-15 09:45:46 +0800

    填充每个节点的下一个右侧节点指针

--- src/tree/PopulatingNextRightPointersInEachNode.java
+++ src/tree/PopulatingNextRightPointersInEachNode.java
@@ -14,17 +14,8 @@ public class PopulatingNextRightPointersInEachNode {
         public Node right;
         public Node next;
 
-        public Node() {}
-
         public Node(int val) {
             this.val = val;
-        }
-
-        public Node(int val, Node left, Node right, Node next) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-            this.next = next;
         }
     }
 
