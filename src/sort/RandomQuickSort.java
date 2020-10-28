61ed3c85a85ae7cac1de38869b973cc8336f1c17
hexi007 <2777125829@qq.com>
2020-10-28 21:33:54 +0800

    随机化快速排序

--- src/sort/RandomQuickSort.java
+++ src/sort/RandomQuickSort.java
@@ -11,6 +11,13 @@ import java.util.Random;
  * create 2020-10-27 21:39
  **/
 public class RandomQuickSort {
+
+    /**
+     *  递归的排序两个区间
+     * @param a 待排序数组
+     * @param left 待排序区间左边界
+     * @param right 待排序区间右边界
+     */
     private void sort(int[] a, int left, int right) {
         if(left < right){
             int p = randomPartition(a, left, right);
@@ -19,12 +26,30 @@ public class RandomQuickSort {
         }
     }
 
+    /**
+     * 随机化待排序区间最右元素，再使基准数移到一个位置从而左边的数都比它小，右边的数都比他大
+     * @param a 待排序数组
+     * @param left 待排序左边界
+     * @param right 待排序右边界
+     * @return 基准位置
+     */
     private int randomPartition(int[] a, int left, int right) {
+        //随机化 right 位置的元素
         int randomIndex = new Random().nextInt(right - left + 1) + left;
         swap(a, randomIndex, right);
 
-        int x = a[right];
-        return x;
+        //x 基准数 ，i 从 left 到 i 存放比基准数小的元素
+        int x = a[right], i = left - 1;
+        for(int j = left; j < right; j++){
+            //如果当前元素比基准数小，就和当前 i 位置的元素交换位置
+            if(a[j] <= x){
+                i++;
+                swap(a, i, j);
+            }
+        }
+        //最后将基准数移到 i 位置上
+        swap(a, ++i ,right);
+        return i;
     }
 
     private void swap(int[] a, int i, int j) {
