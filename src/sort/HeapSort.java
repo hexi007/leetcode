70b409cb8c610094b3b29f69bf836907b008dd3e
hexi007 <2777125829@qq.com>
2020-10-29 11:30:30 +0800

    堆排序

--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -10,14 +10,44 @@ import java.util.Arrays;
  **/
 public class HeapSort {
 
-    private void sort(int[] a, int i, int i1) {
-         for(int i = a.length / 2 - 1; i < )
+    private void sort(int[] a) {
+         int half = 2;
+         for(int i = a.length / half - 1; i >= 0; i--){
+            //调整大顶堆
+            adjustHeap(a, i, a.length);
+         }
+         for(int i = a.length - 1; i > 0; i--){
+             swap(a, 0, i);
+             adjustHeap(a, 0, i);
+         }
+    }
+
+    private void adjustHeap(int[] a, int index, int length) {
+        int temp = a[index], doubleK = 2;
+        for(int k = doubleK * index + 1; k < length; k = doubleK * k + 1){
+            if(k + 1 < length && a[k] < a[k + 1]){
+                k++;
+            }
+            if(a[k] > a[index]){
+                a[index] = a[k];
+                index = k;
+            } else {
+                break;
+            }
+        }
+        a[index] = temp;
+    }
+
+    private void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 
     public static void main(String[] args) {
         int[] a = {13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11};
         HeapSort rq = new HeapSort();
-        rq.sort(a, 0, a.length - 1);
+        rq.sort(a);
 
         System.out.println(Arrays.toString(a));
     }
