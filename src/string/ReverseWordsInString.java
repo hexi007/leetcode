22f712ce4ef098ff5957f14626f509e3e67422ce
hexi007 <2777125829@qq.com>
2020-11-10 16:09:38 +0800

    翻转字符串里的单词

--- src/string/ReverseWordsInString.java
+++ src/string/ReverseWordsInString.java
@@ -1,5 +1,10 @@
 package string;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * description 给定一个字符串，逐个翻转字符串中的每个单词。
  * 说明：  无空格字符构成一个 单词 。 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
@@ -20,9 +25,6 @@ public class ReverseWordsInString {
          */
         public String reverseWords(String s) {
             String[] strings = s.split("\\s+ ");
-            for (String string : strings) {
-                System.out.println(string);
-            }
             for (int i = 0; i < strings.length / 2; i++) {
                 String temp = strings[i];
                 strings[i] = strings[strings.length - i - 1];
@@ -38,10 +40,46 @@ public class ReverseWordsInString {
             String res = sb.toString();
             return res.substring(0, res.length() - 1);
         }
+
+        /**
+         * 手动拆分字符串，在逆序构造字符串
+         * (执行用时：3 ms, 在所有 Java 提交中击败了81.56% 的用户)
+         * (内存消耗：38.4 MB, 在所有 Java 提交中击败了94.53% 的用户)
+         * @param s 字符串
+         * @return  逐个翻转每个单词后的字符串
+         */
+        public String reverseWords1(String s) {
+            List<String> list = new ArrayList<>();
+            char[] chars = s.toCharArray();
+            int len = chars.length;
+            int left = 0;
+            while (left < len) {
+                while (left < len && chars[left] == ' ') {
+                    left++;
+                }
+                int right = left + 1;
+                while (right < len && chars[right] != ' ') {
+                    right++;
+                }
+                if (left < len) {
+                    list.add(s.substring(left, right));
+                }
+                left = right + 1;
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = list.size() - 1; i >= 0; i--) {
+                if ( i != 0) {
+                    stringBuilder.append(list.get(i)).append(" ");
+                } else {
+                    stringBuilder.append(list.get(i));
+                }
+            }
+            return stringBuilder.toString();
+        }
     }
 
     public static void main(String[] args) {
-        String s = "  hello    world!     ";
-        System.out.println(new Solution().reverseWords(s));
+        String s = "    the   ";
+        System.out.println(new Solution().reverseWords1(s));
     }
 }
