73190f66a248bf2e294d016c36bfc9a158905a9a
hexi007 <2777125829@qq.com>
2020-11-26 10:54:35 +0800

    二叉树展开为链表

--- src/tree/FlattenBinaryTreeToLinkedList.java
+++ src/tree/FlattenBinaryTreeToLinkedList.java
@@ -11,21 +11,34 @@ import java.util.List;
 public class FlattenBinaryTreeToLinkedList {
 
     static class Solution {
+
+        /**
+         * 递归，先展开左孩子链表，再拼接右孩子链表
+         * (执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户)
+         * (内存消耗：38.2 MB, 在所有 Java 提交中击败了60.14%的用户)
+         *
+         * @param root 根节点
+         */
         public void flatten(TreeNode root) {
             if (root == null) {
                 return;
             }
-            if (root.left == null && root.right == null) {
-                return;
+            // 展开左孩子链表,之后可以将左孩子看成已展开的链表
+            flatten(root.left);
+            // 保存右孩子节点
+            TreeNode right = root.right;
+            // 左孩子已到右孩子
+            root.right = root.left;
+            // 左孩子置为空
+            root.left = null;
+            // 不断寻找 右孩子节点插入位置
+            while (root.right != null) {
+                root = root.right;
             }
-            if (root.left == null) {
-                return;
-            }
-            myFlatten(root.right);
-        }
-
-        private TreeNode myFlatten(TreeNode root) {
-            return root;
+            // 插入右孩子
+            root.right = right;
+            // 再展开右孩子链表
+            flatten(root.right);
         }
     }
 
