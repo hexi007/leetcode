5614808bd706a5f4edf6458f08c91b10257b954b
hexi007 <2777125829@qq.com>
2021-01-13 19:54:33 +0800

    冗余连接

--- src/union_finddisjointsets/RedundantConnection.java
+++ src/union_finddisjointsets/RedundantConnection.java
@@ -1,24 +1,43 @@
-import java.util.Stack;
+package union_finddisjointsets;
+
+import java.util.Arrays;
 
 /**
+ * description 在本问题中, 树指的是一个连通且无环的无向图。
+ * 输入一个图，该图由一个有着N个节点 (节点值不重复1, 2, ..., N) 的树及一条附加的边构成。
+ * 附加的边的两个顶点包含在1到N中间，这条附加的边不属于树中已存在的边。  结果图是一个以边组成的二维数组。
+ * 每一个边的元素是一对[u, v] ，满足 u < v，表示连接顶点u 和v的无向图的边。
+ * 返回一条可以删去的边，使得结果图是一个有着N个节点的树。如果有多个答案，则返回二维数组中最后出现的边。
+ * 答案边 [u, v] 应满足相同的格式 u < v。
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/redundant-connection 著作权归领扣网络所有。
+ * 商业转载请联系官方授权，非商业转载请注明出处。
  *
  * @author 27771
- * create 2021-01-013 15:03
+ * create 2021-01-13 19:43
  **/
-public class NumberOfProvinces {
+public class RedundantConnection {
 
     static class Solution {
 
+        /**
+         * 并查集连接没有相连的点，出现重复就返回结果
+         * (执行用时：1 ms, 在所有 Java 提交中击败了87.99%的用户)
+         * (内存消耗：38.8 MB, 在所有 Java 提交中击败了38.31%的用户)
+         *
+         * @param edges 图的所有边
+         * @return      一条附加的边
+         */
         public int[] findRedundantConnection(int[][] edges) {
             int n = edges.length;
+            // 图顶点从 1 开始标号
             UnionFind unionFind = new UnionFind(n + 1);
             for (int[] edge : edges) {
-				if (!unionFind.isConnected(edge[0], edge[1])) {
-					unionFind.joinElement(edge[0], edge[1]);
-				} else {
-					return edge;
-				}
-			}
+                if (!unionFind.isConnected(edge[0], edge[1])) {
+                    unionFind.joinElement(edge[0], edge[1]);
+                } else {
+                    return edge;
+                }
+            }
             return null;
         }
 
@@ -84,17 +103,17 @@ public class NumberOfProvinces {
                     weight[secondRoot] += weight[firstRoot];
                 }
             }
-			
-			/**
-         * 判断两个元素是否在同一个集合
-         * @param firstElement  元素 1
-         * @param secondElement 元素 2
-         * @return              是否在一个集合
-         */
-        public boolean isConnected(int firstElement, int secondElement) {
-            // 通过两元素最终父节点是否是同一个
-            return find(firstElement) == find(secondElement);
-        }
+
+            /**
+             * 判断两个元素是否在同一个集合
+             * @param firstElement  元素 1
+             * @param secondElement 元素 2
+             * @return              是否在一个集合
+             */
+            public boolean isConnected(int firstElement, int secondElement) {
+                // 通过两元素最终父节点是否是同一个
+                return find(firstElement) == find(secondElement);
+            }
 
             /**
              * 统计并查集有多少个集合
@@ -116,6 +135,6 @@ public class NumberOfProvinces {
 
     public static void main(String[] args) {
         int[][] edges = {{1,2},{1,3},{2,3}};
-        new Solution().findRedundantConnection(edges);
+        System.out.println(Arrays.toString(new Solution().findRedundantConnection(edges)));
     }
 }
