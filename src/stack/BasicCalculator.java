c5f319b10ed9af6c8d96bcb68ab8b0343a6651e4
hexi007 <2777125829@qq.com>
2021-03-10 09:51:53 +0800

    基本计算器

--- src/stack/BasicCalculator.java
+++ src/stack/BasicCalculator.java
@@ -67,10 +67,67 @@ public class BasicCalculator {
 
             return res;
         }
+
+        int[] arrayStack;
+        int stackIndex = 0;
+
+        /**
+         * 数组模拟栈实现
+         * (执行用时：7 ms, 在所有 Java 提交中击败了93.33%的用户)
+         * (内存消耗：38.6 MB, 在所有 Java 提交中击败了68.25%的用户)
+         *
+         * @param s 字符串表达式
+         * @return  字符串表达式的值
+         */
+        public int calculate1(String s) {
+            // sign 表示正负号 res 保存结果
+            int sign = 1, res = 0;
+            chars = s.toCharArray();
+            int len = chars.length;
+            arrayStack = new int[len];
+
+            for (int  i = 0; i  < len; i++) {
+                char c = chars[i];
+                if (Character.isDigit(c)) {
+                    // 当前字符是数字
+                    int temp = c - '0';
+                    // 取出完整数字
+                    while (i + 1 < len && Character.isDigit(chars[i + 1])) {
+                        temp = temp * 10 + chars[++i] - '0';
+                    }
+                    // 根据正负号保存 res
+                    res = res + sign * temp;
+                } else if (c == '+') {
+                    // 修正正负号
+                    sign = 1;
+                } else if (c == '-') {
+                    // 修正正负号
+                    sign = -1;
+                } else if (c == '(') {
+                    // 遇到左括号，保存 res
+                    arrayStack[stackIndex++] = res;
+                    // 为了计算括号内结果，res 置零
+                    res = 0;
+                    // 保存正负号
+                    arrayStack[stackIndex++] = sign;
+                    // 括号开始时默认正负号为正
+                    sign = 1;
+                } else if (c == ')') {
+                    // 遇到右括号计算括号内的值（stack.pop() * res）和 之前保存的值之和
+                    // 其中一开始取的栈顶是正负号， 所以 stack.pop() * res 是括号内的值
+                    // 下一个 stack.pop() 是括号之前保存的结果
+                    int pop = arrayStack[--stackIndex];
+                    res = pop * res + arrayStack[--stackIndex];
+                }
+            }
+
+            return res;
+        }
     }
 
     public static void main(String[] args) {
         String s = "(1+(4+5+2)-3)+(6+8)";
         System.out.println(new Solution().calculate(s));
+        System.out.println(new Solution().calculate1(s));
     }
 }
