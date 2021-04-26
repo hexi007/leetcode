95ba1dca3a08a4cf2f924cfeb3c49e6e627286d1
hexi007 <2777125829@qq.com>
2021-04-26 10:58:42 +0800

    在 D 天内送达包裹的能力

--- src/binarysearch/CapacityToShipPackages.java
+++ src/binarysearch/CapacityToShipPackages.java
@@ -1,6 +1,5 @@
 package binarysearch;
 
-import java.util.Collections;
 import java.util.PriorityQueue;
 
 /**
@@ -16,24 +15,68 @@ import java.util.PriorityQueue;
 public class CapacityToShipPackages {
 
     static class Solution {
+
+        /**
+         * 确定船最低和最高运载能力，再二分查找
+         * (执行用时：8 ms, 在所有 Java 提交中击败了98%的用户)
+         * (内存消耗：41.5 MB, 在所有 Java 提交中击败了93.58%的用户)
+         *
+         * @param weights 包裹的重量数组
+         * @param d       目标天数
+         * @return        在 D 天内将传送带上的所有包裹送达的船的最低运载能力
+         */
         public int shipWithinDays(int[] weights, int d) {
             int len = weights.length;
 
-            System.out.println(len % d == 0 ? len / d : len / d + 1);
-            PriorityQueue<Integer> queue = new PriorityQueue<>(2,
-                    Collections.reverseOrder());
+            // 船运载能力至少要比最重的货物还要大
+            int left = 0;
             for (int weight : weights) {
-                queue.offer(weight);
+                left = Math.max(left, weight);
             }
-            System.out.println(queue);
 
-            return 0;
+            // len 个货物, d 天送走, 那么至少一次要送 len / d（向上取整）个货物
+            // 让最重的 len / d 个货物一次性送完， right 就是这几个最重之和
+            PriorityQueue<Integer> queue = new PriorityQueue<>();
+            int queueSize = len % d == 0 ? len / d : len / d + 1;
+            for (int i = 0; i < queueSize; i++) {
+                queue.offer(weights[i]);
+            }
+            for (int i = queueSize; i < len; i++) {
+                if (!queue.isEmpty() && queue.peek() < weights[i]) {
+                    queue.poll();
+                    queue.offer(weights[i]);
+                }
+            }
+            int right = 0;
+            for (int weight : queue) {
+                right += weight;
+            }
+
+            // 二分查找
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                int tempWeight = 0, dayCount = 1;
+                for (int weight : weights) {
+                    if (tempWeight + weight > mid) {
+                        dayCount++;
+                        tempWeight = 0;
+                    }
+                    tempWeight += weight;
+                }
+                if (dayCount <= d) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
         }
     }
 
     public static void main(String[] args) {
-        int[] weights = {1,2,3,4,5,6,7,8,9,10};
-        int d = 5;
+        int[] weights = {1,2,3,1,1};
+        int d = 4;
         System.out.println(new Solution().shipWithinDays(weights, d));
     }
 }
