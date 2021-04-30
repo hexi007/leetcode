43264abc8d11611124320e7e78055ef3a94f4eef
hexi007 <2777125829@qq.com>
2021-04-30 10:21:59 +0800

    只出现一次的数字 II

--- src/array/SingleNumberTwo.java
+++ src/array/SingleNumberTwo.java
@@ -37,10 +37,37 @@ public class SingleNumberTwo {
 
             return 0;
         }
+
+        /**
+         * 每个数字长 32 位，统计每位 1 的个数，如果不能整除 3 表示那个单独出现的数在这一位 是 1
+         * 找出所有这样的 1 ，最后结果就是 只出现了一次的元素
+         * (执行用时：4 ms, 在所有 Java 提交中击败了37.31%的用户)
+         * (内存消耗：38.2 MB, 在所有 Java 提交中击败了81.99%的用户)
+         *
+         * @param nums 非空整数数组
+         * @return     只出现了一次的元素
+         */
+        public int singleNumber1(int[] nums) {
+            int res = 0, wordSize = 32;
+            for (int i = 0; i < wordSize; i++) {
+                int flag = 1 << i, count = 0;
+                for (int num : nums) {
+                    if ((num & flag) != 0) {
+                        count++;
+                    }
+                }
+                if (count % 3 != 0) {
+                    res |= flag;
+                }
+            }
+
+            return res;
+        }
     }
 
     public static void main(String[] args) {
         int[] nums = {1,1,1,0};
         System.out.println(new Solution().singleNumber(nums));
+        System.out.println(new Solution().singleNumber1(nums));
     }
 }
