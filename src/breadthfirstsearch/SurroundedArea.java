99d8cfd74304e125c13ac9852a1cbecfb70cc986
hexi007 <2777125829@qq.com>
2021-09-24 12:11:13 +0800

    被围绕的区域

--- src/breadthfirstsearch/SurroundedArea.java
+++ src/breadthfirstsearch/SurroundedArea.java
@@ -35,9 +35,10 @@ public class SurroundedArea {
             visited[i][j] = true;
             Deque<int[]> queue = new LinkedList<>();
             queue.add(new int[]{i, j});
-            int[][] directions = {{-1, 0}, {1, 0}, {-1, 0}, {1, 0}};
+            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
             List<int[]> list = new ArrayList<>();
             list.add(new int[]{i, j});
+            boolean isSurrounded = true;
             while (!queue.isEmpty()) {
                 int size = queue.size();
                 for (int k = 0; k < size; k++) {
@@ -45,8 +46,11 @@ public class SurroundedArea {
                     for (int[] direction : directions) {
                         int x = index[0] + direction[0];
                         int y = index[1] + direction[1];
-                        if (x < 0 || x >= rows || y < 0 || y >= cols
-                                || board[x][y] == 'X' || visited[x][j]) {
+                        if (x < 0 || x >= rows || y < 0 || y >= cols) {
+                            isSurrounded = false;
+                            continue;
+                        }
+                        if (board[x][y] == 'X' || visited[x][y]) {
                             continue;
                         }
                         queue.add(new int[]{x, y});
@@ -55,8 +59,67 @@ public class SurroundedArea {
                     }
                 }
             }
-            for(int[] index : list) {
-                System.out.println(Arrays.toString(index));
+            if (isSurrounded) {
+                for (int[] index : list) {
+                    board[index[0]][index[1]] = 'X';
+                }
+            }
+        }
+
+        public void solve1(char[][] board) {
+            if (board == null || board.length == 0) {
+                return;
+            }
+            rows = board.length;
+            cols = board[0].length;
+            for (int i = 0; i < cols; i++) {
+                if (board[0][i] == 'O') {
+                    bfs(board, 0, i);
+                }
+                if (board[rows - 1][i] == 'O') {
+                    bfs(board, rows - 1, i);
+                }
+            }
+            for (int i = 0; i < rows; i++) {
+                if (board[i][0] == 'O') {
+                    bfs(board, i, 0);
+                }
+                if (board[i][cols - 1] == 'O') {
+                    bfs(board, i, cols - 1);
+                }
+            }
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    if (board[i][j] == 'O') {
+                        board[i][j] = 'X';
+                    }
+                    if (board[i][j] == 'Q') {
+                        board[i][j] = 'O';
+                    }
+                }
+            }
+        }
+
+        private void bfs(char[][] board, int i, int j) {
+            Deque<int[]> deque = new LinkedList<>();
+            deque.push(new int[]{i, j});
+            board[i][j] = 'Q';
+            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+            while (!deque.isEmpty()) {
+                int size = deque.size();
+                for (int k = 0; k < size; k++) {
+                    int[] index = deque.pop();
+                    for (int[] direction : directions) {
+                        int x = index[0] + direction[0];
+                        int y = index[1] + direction[1];
+                        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length
+                                || board[x][y] == 'X' || board[x][y] == 'Q') {
+                            continue;
+                        }
+                        board[x][y] = 'Q';
+                        deque.push(new int[]{x, y});
+                    }
+                }
             }
         }
     }
@@ -66,6 +129,7 @@ public class SurroundedArea {
                 {'X', 'X', 'O', 'X'}, {'X', 'O', 'X', 'X'}};
         Solution solution = new Solution();
         solution.solve(board);
+        solution.solve1(board.clone());
         for (char[] c : board) {
             System.out.println(c);
         }
